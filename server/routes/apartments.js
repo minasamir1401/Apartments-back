9784ffa81795194ac6f49b26f05ef911a6fe67a7
minasamir1401 <mina15g4y@gmail.com>
2026-04-05 16:32:52 +0200

    Fix image upload overwrite & add local storage for bookings

--- server/routes/apartments.js
+++ server/routes/apartments.js
@@ -11,7 +11,8 @@ const storage = multer.diskStorage({
     cb(null, path.join(__dirname, '../../uploads/'));
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + '-' + file.originalname);
   }
 });
 
--- server/routes/hero.js
+++ server/routes/hero.js
@@ -11,7 +11,8 @@ const storage = multer.diskStorage({
     cb(null, 'uploads/');
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
   }
 });
 
